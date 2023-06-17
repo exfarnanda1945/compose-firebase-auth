6487e5329332743ae5365652f38bbbc0d5df5ebc
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-17 22:34:47 +0700

    setup navigation graph

--- app/src/main/java/com/example/composefirebaseauth/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composefirebaseauth/presentation/MainActivity.kt
@@ -6,15 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.composefirebaseauth.ui.theme.ComposeFirebaseAuthTheme
+import com.example.composefirebaseauth.utils.navigation.Routes
+import com.example.composefirebaseauth.utils.navigation.SetupNavGraph
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val startDestination = if (firebaseAuth.currentUser != null) {
+            Routes.HomeScreen.route
+        } else {
+            Routes.LoginScreen.route
+        }
         setContent {
             ComposeFirebaseAuthTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +35,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navHostController = rememberNavController()
+                    SetupNavGraph(
+                        navHostController = navHostController,
+                        startDestination = startDestination
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeFirebaseAuthTheme {
-        Greeting("Android")
     }
 }
