8465347d784b09c038fe09b9fa9fd9b56be808d9
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-06-17 22:28:01 +0700

    add dialog loading component

---
+++ app/src/main/java/com/example/composefirebaseauth/presentation/components/DialogLoading.kt
@@ -0,0 +1,38 @@
+package com.example.composefirebaseauth.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun DialogLoading() {
+    Dialog(
+        onDismissRequest = {},
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.White, shape = RoundedCornerShape(8.dp))
+        ) {
+            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                CircularProgressIndicator()
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Loading")
+            }
+        }
+    }
+}
